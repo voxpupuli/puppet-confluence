30e142839a158c20d9bfb65856c83a9f3ab7c80e
Merritt Krakowitzer <merritt@krakowitzer.com>
2015-02-21 19:45:47 +0200

    Add facts to make strict variables pass

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,7 +2,11 @@
 #
 # Install confluence, See README.md for more.
 #
-class confluence::service {
+class confluence::service(
+  $service_file_location = $params::service_file_location,
+  $service_file_template = $params::service_file_template,
+  $service_lockfile      = $params::service_lockfile,
+)  {
   if $confluence::manage_service {
     service { 'confluence':
       ensure  => 'running',
