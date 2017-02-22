96c45b3012f1a71694cb620f9d41a622c639a76d
Ben Ford <ben.ford@puppetlabs.com>
2017-02-22 09:50:45 -0800

    Add the $os['family'] fact to fix test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ end
 require 'puppetlabs_spec_helper/module_spec_helper'
 RSpec.configure do |c|
   c.default_facts = {
+    os: { family: 'Debian' },
     osfamily: 'Debian',
     augeasversion: '1.0.0',
     staging_http_get: 'curl',
