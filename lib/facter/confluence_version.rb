4ac24bec503c98e3c3ae754b34c3c4f3f564d402
senax <frank@crystalconsulting.eu>
2017-03-24 09:51:24 +0000

    updated facter plugin with Rubocop suggestions

--- lib/facter/confluence_version.rb
+++ lib/facter/confluence_version.rb
@@ -2,11 +2,11 @@ Facter.add(:confluence_version) do
   setcode do
     ps = Facter::Util::Resolution.exec('ps ax')
     confluence_process = ps && ps.split("\n").find { |x| x.include?('atlassian-confluence-') }
-    confluence_process =~ /^.*atlassian-confluence-(\d+\.\d+\.\d+).*/
-    if $1.nil?
+    confluence_process =~ %r{^.*atlassian-confluence-(\d+\.\d+\.\d+).*}
+    if Regexp.last_match(1).nil?
       'unknown'
     else
-      $1
+      Regexp.last_match(1)
     end
   end
 end
