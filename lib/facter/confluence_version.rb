2c49b5dcb890a2a3a8ef0b080bb31ab9aad8884e
senax <frank@crystalconsulting.eu>
2017-04-03 09:37:14 +0100

    confluence_version fact now using pgrep

--- lib/facter/confluence_version.rb
+++ lib/facter/confluence_version.rb
@@ -1,12 +1,13 @@
 Facter.add(:confluence_version) do
   setcode do
-    ps = Facter::Util::Resolution.exec('ps ax')
-    confluence_process = ps && ps.split("\n").find { |x| x.include?('atlassian-confluence-') }
-    confluence_process =~ %r{^.*atlassian-confluence-(\d+\.\d+\.\d+).*}
+    pgrep = Facter::Util::Resolution.exec(
+      'pgrep --list-full --full java.*atlassian-confluence-[0-9].*org.apache.catalina.startup.Bootstrap'
+    )
+    pgrep.to_s =~ %r{^.*atlassian-confluence-(\d+\.\d+\.\d+).*}
     if Regexp.last_match(1).nil?
       'unknown'
     else
       Regexp.last_match(1)
-    end
+   end
   end
 end
