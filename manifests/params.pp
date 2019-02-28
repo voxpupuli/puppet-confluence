4cab59dd99c99b1e5021a413d4d80e5b2d9dec22
Christopher Pepe <pepeca@praecipio.com>
2019-02-28 10:13:36 -0500

    added support for Amazon linux 2018 (using sysv not systemd)

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,8 +16,13 @@ class confluence::params {
         $service_file_template = 'confluence/confluence.initscript.erb'
         $service_lockfile      = '/var/lock/subsys/confluence'
         $refresh_systemd       = false
+      } elsif $facts['os']['name'] == 'Amazon' and $facts['os']['release']['major'] == '2018' {
+        $service_file_location = '/etc/init.d/confluence'
+        $service_file_template = 'confluence/confluence.initscript.erb'
+        $service_lockfile      = '/var/lock/subsys/confluence'
+        $refresh_systemd       = false
       } else {
-        fail("Only osfamily ${facts['os']['family']} 6 and 7 and supported")
+        fail("Only osfamily ${facts['os']['family']} 6 and 7, and Amazon Linux 2018 are supported")
       }
     }
     /Debian/: {
