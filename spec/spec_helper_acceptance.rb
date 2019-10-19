f1413f2603ac1652f682185311f9fe644d426170
Tim Meusel <tim@bastelfreak.de>
2019-10-19 15:46:20 +0200

    rubocop: autofix

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -16,11 +16,10 @@ RSpec.configure do |c|
 
     hosts.each do |host|
       on host, 'chmod 755 /root'
-      if fact_on(host, 'osfamily') == 'Debian'
-        on host, "echo \"en_US ISO-8859-1\nen_NG.UTF-8 UTF-8\nen_US.UTF-8 UTF-8\n\" > /etc/locale.gen"
-        on host, '/usr/sbin/locale-gen'
-        on host, '/usr/sbin/update-locale'
-      end
+      next unless fact_on(host, 'osfamily') == 'Debian'
+      on host, "echo \"en_US ISO-8859-1\nen_NG.UTF-8 UTF-8\nen_US.UTF-8 UTF-8\n\" > /etc/locale.gen"
+      on host, '/usr/sbin/locale-gen'
+      on host, '/usr/sbin/update-locale'
     end
   end
 end
