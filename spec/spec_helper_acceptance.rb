3baa859428c01056532fc0293dd2d968e1ebe27b
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-06 23:10:31 +0200

    Use voxpupuli-acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,25 +1,9 @@
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-
-RSpec.configure do |c|
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    install_module
-    install_module_dependencies
-
-    hosts.each do |host|
-      on host, 'chmod 755 /root'
-      next unless fact_on(host, 'osfamily') == 'Debian'
-      on host, "echo \"en_US ISO-8859-1\nen_NG.UTF-8 UTF-8\nen_US.UTF-8 UTF-8\n\" > /etc/locale.gen"
-      on host, '/usr/sbin/locale-gen'
-      on host, '/usr/sbin/update-locale'
-    end
-  end
+configure_beaker do |host|
+  on host, 'chmod 755 /root'
+  next unless fact_on(host, 'osfamily') == 'Debian'
+  on host, "echo \"en_US ISO-8859-1\nen_NG.UTF-8 UTF-8\nen_US.UTF-8 UTF-8\n\" > /etc/locale.gen"
+  on host, '/usr/sbin/locale-gen'
+  on host, '/usr/sbin/update-locale'
 end
